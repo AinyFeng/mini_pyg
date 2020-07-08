db6bb63f29817de885c1f15530ebe1e36467bfd4
anxiaofeng <1263606186@qq.com>
2020-07-08 16:44:13 +0800

    商品详情的界面编辑及获取地址的api的调用

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,74 @@
-// pages/cart/index.js
+/**
+ 1. 获取用户的收货地址
+   1. 绑定点击事件
+   2. 调用小程序内置 api 获取用户的收货地址 chooseAddress, 如果用户选择了取消,下次再获取就没有反应
+  2. 获取用户 对小程序 所授予 获取地址的 权限 状态 scope
+      1. 假设 用户 点击获取收货地址的提示框 确定 authSetting  scope.address 
+        scope 值 true   直接调用 获取收货地址
+      2. 假设 用户 从来没有调用过 收货地址的 API
+        scope undefined 直接调用 获取收货地址
+      3. 假设 用户 点击获取收货地址的提示框 取消
+        scope 值 false
+        1. 诱导用户 自己 打开 授权设置页面 当用户重新给与 获取地址权限的时候
+        2. 获取收货地址
+ */
+import {getSetting, chooseAddress, openSetting} from "../../utils/asyncWx.js"
+import regeneratorRuntime from '../../lib/runtime/runtime.js';
+
 Page({
+  // 点击收货地址
+  async handleChooseAddress() {
+    // console.log('点击了');
+    // 获取收货地址
+    // wx.chooseAddress({
+    //   success: (result)=>{
+    //     console.log(result);
+    //   }
+    // });
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    // TODO 以下代码可以通过下面的代码封装的替代
+    // 1. 获取 权限状态
+    // wx.getSetting({
+    //   success: (result)=>{
+    //     // 2. 获取权限状态  主要发现一些 属性名很怪异的时候 都要使用[]形式来获取属性值
+    //     const scopeAddress = result.authSetting["scope.address"];
+    //     if (scopeAddress === true || scopeAddress === undefined) {
+    //       wx.chooseAddress({
+    //         success: (result1)=>{
+    //           console.log(result1);
+    //         }
+    //       });
+    //     }else {
+    //       // 3. 用户 以前拒绝过授权权限 先诱导用户打开授权页面
+    //       wx.openSetting({
+    //         success: (result2)=>{
+    //           // 4. 可以调用 收货地址代码
+    //           wx.chooseAddress({
+    //             success: (result3)=>{
+    //               console.log(result3);
+    //             }
+    //           });
+    //         }
+    //       });
+    //     }
+    //   },
+    //   fail: ()=>{},
+    //   complete: ()=>{}
+    // });
 
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 1. 获取 权限状态
+    const res1 = await getSetting();
+    const scopeAddress = res1.authSetting["scope.address"];
+    // 2. 判断 权限状态
+    if (scopeAddress === true || scopeAddress === undefined) {
+      // 3. 调用获取收货地址的 api
+      const res2 = await chooseAddress();
+    } else {
+      // 4. 先诱导用户打开授权页面
+      await openSetting();
+      // 5. 调用获取收货地址的 api
+      const res3 = await chooseAddress();
+    }
+    
   }
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,3 +1,16 @@
+/**
+ * 1. 发送请求获取数据
+ * 2. 点击轮播图 预览大图
+ *     1. 给轮播图绑定点击事件
+ *     2. 调用小程序的api previewImage
+ * 3. 点击加入购物车
+ *  1. 先绑定点击事件
+ *  2. 获取缓存中的购物车数据 数组格式
+ *  3. 先判断 当前的商品是否已经存在于 购物车
+ *  4. 已经存在 修改商品数据 执行购物车数量++ 重新把购物车数组 填充回缓村中
+ *  5. 不存在于购物车的数组中 直接给购物车数组添加一个新的元素 新元素 带上 购买数量属性 num 重新把购物车数组 填充回缓存中
+ *  6. 弹出提示
+ */
 import { request } from "../../request/index.js";
 import regeneratorRuntime from '../../lib/runtime/runtime.js';
 Page({
@@ -10,6 +23,8 @@ Page({
     goodsObj: {}
 
   },
+  // 商品对象
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
@@ -22,8 +37,53 @@ Page({
   async getGoodsDetail(goods_id) {
     const goodsObj = await request({url: '/goods/detail', data: { goods_id }});
     console.log(goodsObj)
+    this.GoodsInfo = goodsObj;
     this.setData({
-      goodsObj
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        // iphone部分手机 不识别 webp图片格式
+        // 最好找到后台 让他进行修改
+        // 临时自己改 确保后台存在 1.webp => 1.jpg
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
+      }
+    })
+  },
+  // 点击轮播图 放大预览
+  handlePreviewImage(e) {
+    // console.log(e);
+    // 1. 先构造要预览的图片数组
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
+    console.log('urls', urls);
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      current, // 当前显示图片的http链接
+      urls // 需要预览的图片http链接列表
+    })
+  },
+  // 点击加入购物车
+  handleCartAdd(e) {
+    // 1. 获取缓存中的购物车 数组
+    let cart = wx.getStorageSync('cart') || [];
+    // 2. 判断 商品对象是否存在于购物车数组中
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      // 3. 不存在 第一次添加
+      this.GoodsInfo.num = 1;
+      cart.push(this.GoodsInfo);
+    }else {
+      // 4. 已经存在购物车数据, 执行 num++
+      cart[index].num++;
+    }
+    // 5.把购物车重新添回缓存中
+    wx.setStorageSync('cart', cart);
+    // 6. 弹窗提示
+    wx.showToast({
+      title: "加入成功",
+      icon: "'success",
+      // true 防止用户 手抖 疯狂点击
+      mask: true
     })
   }
 })
