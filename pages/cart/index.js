fef35257e7a9609fd5fd3f254beca11637b68626
anxiaofeng <1263606186@qq.com>
2020-07-08 18:16:11 +0800

    购物车的界面的编写

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -11,11 +11,27 @@
         scope 值 false
         1. 诱导用户 自己 打开 授权设置页面 当用户重新给与 获取地址权限的时候
         2. 获取收货地址
+      4. 把获取到的收货地址 存入到 本地存储中
+2. 页面加载完毕
+0 onLoad onShow
+   1. 获取本地存储中的地址数据
+   2. 把数据 设置给data中的一个变量
  */
 import {getSetting, chooseAddress, openSetting} from "../../utils/asyncWx.js"
 import regeneratorRuntime from '../../lib/runtime/runtime.js';
 
 Page({
+  data: {
+    address: {}
+  },
+  onShow() {
+    // 1. 获取缓存中的收货地址信息
+    const address = wx.getStorageSync("address");
+    // 2. 给data赋值
+    this.setData({
+      address
+    })
+  },
   // 点击收货地址
   async handleChooseAddress() {
     // console.log('点击了');
@@ -55,19 +71,39 @@ Page({
     //   fail: ()=>{},
     //   complete: ()=>{}
     // });
-
+    /**
+     优化一
+     */
     // 1. 获取 权限状态
-    const res1 = await getSetting();
-    const scopeAddress = res1.authSetting["scope.address"];
-    // 2. 判断 权限状态
-    if (scopeAddress === true || scopeAddress === undefined) {
-      // 3. 调用获取收货地址的 api
-      const res2 = await chooseAddress();
-    } else {
-      // 4. 先诱导用户打开授权页面
-      await openSetting();
-      // 5. 调用获取收货地址的 api
-      const res3 = await chooseAddress();
+    // const res1 = await getSetting();
+    // const scopeAddress = res1.authSetting["scope.address"];
+    // // 2. 判断 权限状态
+    // if (scopeAddress === true || scopeAddress === undefined) {
+    //   // 3. 调用获取收货地址的 api
+    //   const res2 = await chooseAddress();
+    // } else {
+    //   // 4. 先诱导用户打开授权页面
+    //   await openSetting();
+    //   // 5. 调用获取收货地址的 api
+    //   const res3 = await chooseAddress();
+    // }
+
+    /**
+     * 代码最优化
+     */
+    try {
+      const res1 = await getSetting();
+      const scopeAddress = res1.authSetting["scope.address"];
+      // 2. 判断 权限状态
+      if (scopeAddress === false) {
+        await openSetting();
+      }
+      let address = await chooseAddress();
+      address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo;
+      // 存入到缓存中
+      wx.setStorageSync('address', address);
+    } catch (error) {
+      console.log(error)
     }
     
   }
