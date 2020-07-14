f622017646c2763f1241b4289e29456d61baed6b
anxiaofeng <1263606186@qq.com>
2020-07-14 10:07:26 +0800

    搜索中心的界面的完善

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,61 @@
-// pages/search/index.js
+/**
+ 1. 输入框绑定 值改变事件 input事件
+    1. 获取到输入框的值
+    2. 合法性判断
+    3. 检验通过 把输入框的值 发送到后台
+    4. 返回的数据打印到页面上
+2. 防抖(防止抖动) 定时器  节流
+    0. 防抖 一般 输入框中 防止重复输入 重复发送请求
+    1. 节流 一般是用在页面下拉和上拉 比如页面的上拉加载下一页和下拉刷新
+    1. 定义全局的定时器id
+
+ */
+import {request} from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goods: [],
+    // 取消按钮是否显示
+    isFocus: false,
+    // 输入框的值
+    inputValue:'',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  TimeId: -1,
+  // 输入框的值改变 就会触发的事件
+  handleInput(e) {
+    // console.log(e);
+    // 1. 获取输入框的值
+    const {value} = e.detail;
+    // 2. 检查合法性
+    if (!value.trim())  {
+      // 不合法性
+      this.setData({
+        goods: [],
+        isFocus: false
+      });
+      return;
+    }
+    // 3. 准备发送请求获取数据
+    this.setData({
+      isFocus: true
+    })
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+       this.qsearch(value);
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 发送请求获取搜索建议 数据
+  async qsearch(query) {
+    const res = await request({url: "/goods/qsearch", data: {query}});
+    this.setData({
+      goods: res
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 取消按钮 事件
+  handleCancel() {
+    this.setData({
+      inputValue: '',
+      goods: [],
+      isFocus: false
+    });
   }
 })
